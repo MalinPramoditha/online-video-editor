8522e7519621833b7bf8b7fa57a27f096a165574
malinpro <malinsa@goproconsultancy.com>
2024-11-24 12:41:57 +0530

    try to run online

---
+++ app/edit/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Page() {
+  return (
+    <div>
+      <video controls src='"https://rr8---sn-uxax4vopj5qx-cxge.googlevideo.com/videoplayback?expire=1732447662&ei=TrlCZ_TSNIiG6dsPvbiE6QQ&ip=176.6.135.178&id=o-AL2-ApQvnmhj5RkkPH9uiyflRxFSbcw7tezK5RpWwiiZ&itag=136&aitags=133%2C134%2C135%2C136%2C137%2C160%2C242%2C243%2C244%2C247%2C248%2C271%2C278%2C313%2C394%2C395%2C396%2C397%2C398%2C399%2C400%2C401&source=youtube&requiressl=yes&xpc=EgVo2aDSNQ%3D%3D&met=1732426062%2C&mh=va&mm=31%2C29&mn=sn-uxax4vopj5qx-cxge%2Csn-4g5edndl&ms=au%2Crdu&mv=m&mvi=8&pl=17&rms=au%2Cau&initcwndbps=1318750&bui=AQn3pFQyO4h5KrS7ZlayBqj-jIhZf8QE_NG_Fa8K2bw1F5aQrafbR50_IxCTPBKZck4xUaT3Hdz9SxyG&spc=qtApAWXU3oqrqn3R3vgEpKMUO04YY3_0IsadlBGU6dDiTPMv-w&vprv=1&svpuc=1&mime=video%2Fmp4&ns=fm-81drJzhdtwMvfXY7EmgQQ&rqh=1&gir=yes&clen=25413749&dur=197.600&lmt=1729734200078899&mt=1732425666&fvip=4&keepalive=yes&fexp=51326932%2C51335594&c=WEB&sefc=1&txp=4532434&n=qw4ijXL4ZzG0xg&sparams=expire%2Cei%2Cip%2Cid%2Caitags%2Csource%2Crequiressl%2Cxpc%2Cbui%2Cspc%2Cvprv%2Csvpuc%2Cmime%2Cns%2Crqh%2Cgir%2Cclen%2Cdur%2Clmt&lsparams=met%2Cmh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl%2Crms%2Cinitcwndbps&lsig=AGluJ3MwRAIgOmwB7o8gXU8axBRq1yZUnXalks5Lrh-OSFqP--iilyMCIFBWM_bI73_3k3vhowUXgM4d07W-F5XBR19ueU2T4R1n&sig=AJfQdSswRQIhANKvOPktzD22uXbSlkW8H97C7QU9vOrjoHFWRQCiS5FSAiA3wMO35gZ_bOEOnIMpMz5p2A-9b5SovzARTcICQM_0PQ%3D%3D"'/>
+    </div>
+  )
+}
