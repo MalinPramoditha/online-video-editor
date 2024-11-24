411a63edf2fb875e362b981402d947b558fdc6eb
malinpro <malinsa@goproconsultancy.com>
2024-11-24 12:49:30 +0530

    try to run online 2

--- next.config.js
+++ next.config.js
@@ -2,18 +2,6 @@
 const nextConfig = {
   images: {
     unoptimized: true,
-  },
-  async headers() {
-    return [
-      {
-        source: '/api/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET, POST, PUT, DELETE, OPTIONS' },
-          { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization, Range' },
-        ],
-      },
-    ]
   }
 }
 
